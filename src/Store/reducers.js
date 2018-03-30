036ebf1093202fba43200e03f4ad7673870f1659
Sunil-tc <suniltc@designesthetics.com>
2018-03-30 17:07:34 +0530

    Updates

--- src/Store/reducers.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from 'redux';
-import { getItemsReducer, getItemReducer } from '../Home/reducers/home';
-
-const rootReducer = combineReducers({
-	getItems: getItemsReducer,
-	getItem: getItemReducer
-});
-
-export default rootReducer;
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import NonVeg from './NonVeg';
-import Spicy from './Spicy';
-
-export default {
-	NonVeg,
-	Spicy
-}
